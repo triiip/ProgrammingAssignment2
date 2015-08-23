c59be2805d28514eb79ae565d3cee4a23b71b8da
Triiip Bowen <triiip@verizon.net>
2015-08-23 12:06:37 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to generate and store an inverse matrix in cache in order to
+## reduce the time to retrieve the inverse matrix. If the inverse matrix is not already in cache,
+## then the inverse matrix is generated.
 
-## Write a short comment describing this function
+# This function establishes a list of functions that are used to get and store the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that receives a matrix, and returns the inverse of that matrix
+## the function checks system cache to determine if the inverse matrix has already been 
+## generated and is in cache. if it is then the inverse matrix is returned from cache. if not,
+## the inverse matrix is generated and returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+ invMat <- x$getSolve()
+  # check to see if the inverse matrix has already been calculated and is in cache,
+  # if so, provide a message back to user that the system is retrieving the data from cache.
+  # then return the inverse matrix
+  
+  if(!is.null(invMat)){
+    message("getting cached data")
+    return (invMat)
+  }
+  # if the inverse matrix is not in cache, provide a message that the inverse matrix is being
+  # generated, then generate the inverse matrix and return it
+  
+  message ("generating inverse matrix")
+  data <- x$get()
+  invMat <-solve(data, ...)
+  x$setSolve(invMat)
+  invMat
 }
+
